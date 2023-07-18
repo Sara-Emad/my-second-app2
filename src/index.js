e9821677c4946f28679c6c26ca6cf8fa6706068f
Sara-Emad <emadsara835@gmail.com>
2023-07-18 11:41:27 +0300

    herf

--- src/index.js
+++ src/index.js
@@ -1,16 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/home' ;
+import Blog from './pages/blog' ;
+import Login from './pages/login';
+import Iwanttohire from './pages/iwanttohire';
+import Iamrecruitr from './pages/iwanttohire';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <h1>sorry............</h1> ,
+  },
+
+  {
+    path: "/Login",
+    element: <Login />,
+  },
+
+  {
+    path: "/Blog",
+    element: <Blog />,
+  },
+
+  {
+    path: "/Iwanttohire",
+    element: <Iwanttohire />,
+  },
+
+
+  {
+    path: "/Iamrecruitr",
+    element: <Iamrecruitr />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
